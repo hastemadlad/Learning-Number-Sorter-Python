7bb4bccead24a38fcd800f16d646f3e4f30fd69a
hastemadlad <maymaanislam@gmail.com>
2025-05-21 02:54:06 +0600

    changed file location and also change email of my github pushing

---
+++ num_sorter.py
@@ -0,0 +1,25 @@
+
+input_decision = str(
+    input("Do you want to sort in Ascending or decending order? (A/D)"))
+input_decision = input_decision.lower()
+
+if input_decision == "a":
+    input_numbers = str(input("Enter the numbers you want to sort--->"))
+    numbers_set = list((input_numbers))
+
+    numbers_set.sort()
+
+    print(numbers_set)
+elif input_decision == "d":
+    input_numbers = str(input("Enter the numbers you want to sort--->"))
+    numbers_set = list((input_numbers))
+
+    numbers_set.sort()
+    numbers_set.reverse()
+
+    print(numbers_set)
+else:
+    print("Please select between only A or D")
+
+
+print("For updates on the best number sorter follow me on github")
